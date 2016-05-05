367db0b56e247d3ba933daa2120899ac0a60d112
abdullah_hylamia <abdullah.hylamia@gmail.com>
2016-05-05 13:00:39 +0200

    imporved line follower and code arrangement

--- test/test.py
+++ test/test.py
@@ -7,32 +7,53 @@ import time
 class Position:
     up, down = 0, -0.15
 
-# def wait_motor(motor):
-#     while (motor.state == ['running']):
-#         pass
+
+def wait_motor(motor):
+    while (motor.state == ['running']):
+        pass
+
+def steering(course, power):
+    """
+    Computes how fast each motor in a pair should turn to achieve the
+    specified steering.
+    Input:
+        course [-100, 100]:
+        * -100 means turn left as fast as possible,
+        *  0   means drive in a straight line, and
+        *  100  means turn right as fast as possible.
+        * If >100 pr = -power
+        * If <100 pl = power
+    power: the power that should be applied to the outmost motor (the one
+        rotating faster). The power of the other motor will be computed
+        automatically.
+    Output:
+        a tuple of power values for a pair of motors.
+    Example:
+        for (motor, power) in zip((left_motor, right_motor), steering(50, 90)):
+            motor.run_forever(speed_sp=power)
+    """
+
+    power_left = power_right = power
+    s = (50 - abs(float(course))) / 50
+
+    if course >= 0:
+        power_right *= s
+        if course > 100:
+            power_right = - power
+    else:
+        power_left *= s
+        if course < -100:
+            power_left = - power
+
+    return (int(power_left), int(power_right))
 
 
-rMtr_sp = 0
-lMtr_sp = 0
+
 mMtr_pos = Position.up
 
 running = True
 
-class WheelsThread(threading.Thread):
-    def __init__(self):
-        self.rMtr = ev3.LargeMotor(ev3.OUTPUT_D)
-        self.lMtr = ev3.LargeMotor(ev3.OUTPUT_C)
-        threading.Thread.__init__(self)
 
-    def run(self):
-        while running:
-            self.rMtr.run_forever(duty_cycle_sp = rMtr_sp)
-            self.lMtr.run_forever(duty_cycle_sp = lMtr_sp)
-
-        self.rMtr.stop()
-        self.lMtr.stop()
-
-## THIS IS A RANDOM COMMENTs AND I ADD THIS
 
 class GripThread(threading.Thread):
     def __init__(self):
@@ -41,12 +62,10 @@ class GripThread(threading.Thread):
 
     def run(self):
         self.mMtr.run_timed(time_sp=1000, duty_cycle_sp=-30)
-        while self.mMtr.state == ['running']:
-            pass
+        wait_motor(self.mMtr)
         self.mMtr.reset()
         self.mMtr.run_to_abs_pos(duty_cycle_sp=30, position_sp=0.25 * self.mMtr.count_per_rot)
-        while self.mMtr.state == ['running']:
-            pass
+        wait_motor(self.mMtr)
         self.mMtr.reset()
         pos_prev = mMtr_pos
         while running:
@@ -55,21 +74,96 @@ class GripThread(threading.Thread):
                 pos_prev = mMtr_pos
         self.mMtr.stop()
 
+class Wheels():
+    def __init__(self):
+        self.rMtr = ev3.LargeMotor(ev3.OUTPUT_D);   assert self.rMtr.connected
+        self.lMtr = ev3.LargeMotor(ev3.OUTPUT_C);   assert self.lMtr.connected
+
+    def start(self):
+        self.rMtr.duty_cycle_sp = 0
+        self.lMtr.duty_cycle_sp = 0
+        self.rMtr.run_direct()
+        self.lMtr.run_direct()
+
+    def stop(self):
+        self.rMtr.stop()
+        self.lMtr.stop()
+
+    def set_sp(self, rm_sp, lm_sp):
+        self.rMtr.duty_cycle_sp = rm_sp
+        self.lMtr.duty_cycle_sp = lm_sp
+
+class ColorSensors():
+    def __init__(self):
+        self.rClr = ev3.ColorSensor(ev3.INPUT_3);   assert self.rClr.connected
+        self.rClr.mode = 'COL-REFLECT'
+        self.lClr = ev3.ColorSensor(ev3.INPUT_1);   assert self.lClr.connected
+        self.lClr.mode = 'COL-REFLECT'
+
+class NavigationThread(threading.Thread):
+    def __init__(self, kp=0.65, kd=1, ki=0.02):
+        self.wheels = Wheels()
+        self.clrs = ColorSensors()
+        self.kp = kp
+        self.kd = kd
+        self.ki = ki
+        self.minRef, self.maxRef, self.target = self.calibrate()
+        threading.Thread.__init__(self)
+
+    def follow_line(self, direction=1, power=60):
+        self.wheels.start()
+        self.following = True
+        lastError = error = integral = 0
+        while self.following:
+            refRead = self.clrs.rClr.value()
+            error = self.target - (100 * (refRead - self.minRef) / (self.maxRef - self.minRef))
+            derivative = error - lastError
+            lastError = error
+            integral = float(0.5) * integral + error
+            course = (self.kp * error + self.kd * derivative + self.ki * integral) * direction
+            self.wheels.set_sp(*steering(course, power))
+            time.sleep(0.01)  # Aprox 100 Hz
+
+    def run(self):
+        self.follow_line()
+
+
+
+    def calibrate(self):
+        min = 255
+        max = 0
+
+        self.wheels.lMtr.run_to_rel_pos(position_sp=0.5*self.wheels.lMtr.count_per_rot, duty_cycle_sp=40)
+        while self.wheels.lMtr.state == ['running']:
+            val = self.clrs.rClr.value()
+            if val > max:
+                max = val
+            if val < min:
+                min = val
+        self.wheels.lMtr.run_to_rel_pos(position_sp=-1*self.wheels.lMtr.count_per_rot, duty_cycle_sp=40)
+        while self.wheels.lMtr.state == ['running']:
+            val = self.clrs.rClr.value()
+            if val > max:
+                max = val
+            if val < min:
+                min = val
+        self.wheels.lMtr.run_to_rel_pos(position_sp=0.52*self.wheels.lMtr.count_per_rot, duty_cycle_sp=40)
+        wait_motor(self.wheels.lMtr)
+        print (min, max, (min+max)/2)
+        return min, max, (min+max)/2
+
+
+
 if __name__ == "__main__":
-    wheel_thread = WheelsThread()
-    wheel_thread.setDaemon(True)
-    wheel_thread.start()
 
     grip_thread = GripThread()
     grip_thread.start()
-
-    time.sleep(4)
-    mMtr_pos = Position.down
-    time.sleep(4)
-    mMtr_pos = Position.up
-    time.sleep(4)
+    nav = NavigationThread()
+    nav.start()
+    time.sleep(5)
 
     running = False
+    nav.following = False
 
 
     # snc = ev3.UltrasonicSensor(ev3.INPUT_4)
