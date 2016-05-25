2c44e56f8b7e2e1fa25cfa312fa3a627ae4ad604
abdullah_hylamia <abdullah.hylamia@gmail.com>
2016-05-26 01:56:34 +0200

    adopted to receive commands throught the gateway

--- robot/robot_control.py
+++ robot/robot_control.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 import zerorpc
-import json
-import time
-from robot import *
-import threading
+import robot
+import sys
 
 
 
@@ -16,7 +14,6 @@ class RobotControl(object):
 
 
     Keyword arguments:
-    object -- The robot object to be controlled
     """
     def __init__(self):
         """
@@ -25,48 +22,15 @@ class RobotControl(object):
         Keyword arguments:
         self -- The class itself.
         """
-        self.rrc = ControlThread()
+        self.rrc = robot.ControlThread()
         self.rrc.start()
         self.rrc.command = 'go-to-start'
 
-    def send_command(self, command, x, y, ID):
-        """
-        Sends a command to the robot.
-
-        Keyword arguments:
-        self -- The object to get the command
-        command -- The command to be run on the object
-        x -- The X-coordinate
-        y -- The Y-coordinate
-        ID --
-        """
-        cmd = ""
-        response = {}
-        cmd_f = True
-
-        if command == "insert":
-            cmd = "insert-box"
-        elif command == "remove":
-            cmd = "remove-box"
-        elif command == "move":
-            cmd = "go-to-location"
-        else:
-            cmd_f = False
-
-        cmd += " %s %s" % (x, y)
-
-        print cmd
-
-        if cmd_f:
-            time.sleep(5)
-            self.rrc.command = cmd
-            while self.rrc.command != "":
-                pass
-
-        response["command"] = command
-        response["ID"] = ID
-        response["status"] = self.rrc.success and cmd_f
-        return "".join(json.dumps(response))
+    def execute_command(self, command):
+        self.rrc.command = command
+        while self.rrc.command != "":
+            pass
+        return self.rrc.success
 
     def stop(self):
         self.rrc.stop()
@@ -76,25 +40,26 @@ class RobotControl(object):
 #
 #
 #
-class ZPC():
+class RobotZRPC():
     def __init__(self):
-        self.waiting = False
         self.rc = RobotControl()
         self.zpc = zerorpc.Server(self.rc)
         self.zpc.bind("tcp://0.0.0.0:4242")
 
     def start(self):
-        threading.Thread(target=self.zpc.run, name='zpc-ctrl')
+        self.zpc.run()
 
     def stop(self):
         self.rc.stop()
         self.zpc.stop()
 
+def main():
+    rzpc = RobotZRPC()
+    try:
+        rzpc.start()
+    except KeyboardInterrupt:
+        rzpc.stop()
+        sys.exit()
 
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
---
+++ robot/start_robot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get file directory
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Get device local IP address
+ip=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+
+# Write IP to a file
+echo $ip > $dir/robot_local_ip.txt
+
+# Start the robot control task
+python $dir/robot_control.py
